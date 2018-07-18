2a010af3012a262e240765ef5b51b5fc62ead9f4
Eli <elimetzlerprieb@gmail.com>
2018-07-18 17:36:40 -0400

    Stub out extension methods for strings and ints to check if palindromic

---
+++ bagOfClasses/PalindromeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bagOfClasses
+{
+    public static class PalindromeExtensions
+    {
+        public static bool isPalindrome(this string str)
+        {
+
+
+
+            return false;
+        }
+
+        public static bool isPalindrome(this int str)
+        {
+
+
+
+            return false;
+        }
+    }
+
+
+}
---
+++ bagOfClassesUnitTests/PalindromeExtensionsTester.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+using bagOfClasses;
+
+namespace bagOfClassesUnitTests
+{
+    public class PalindromeExtensionsTester
+    {
+        [Theory]
+        [InlineData("123321")]
+        [InlineData("ABCCBA")]
+        [InlineData("ABCDCBA")]
+        public void ShouldReturnTrueIfPalindrome(string input)
+        {
+            Assert.True(input.isPalindrome());
+        }
+
+        [Theory]
+        [InlineData(123321)]
+        [InlineData(9003009)]
+        [InlineData(90011009)]
+        [InlineData(12344321)]
+        public void ShouldReturnTrueIfPalindrome(int input)
+        {
+            Assert.True(input.isPalindrome());
+        }
+
+    }
+}
