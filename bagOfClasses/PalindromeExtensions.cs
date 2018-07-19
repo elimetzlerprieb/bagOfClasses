94d8a10004f74ff554b2c5d3ff10c4e7c2038fae
Eli <elimetzlerprieb@gmail.com>
2018-07-19 16:44:50 -0400

    Add function to find largest product of 3 digit integers that is a palindrome

--- bagOfClasses/PalindromeExtensions.cs
+++ bagOfClasses/PalindromeExtensions.cs
@@ -6,9 +6,37 @@ using System.Threading.Tasks;
 
 namespace bagOfClasses
 {
+    public class LargestPalindrome
+    {
+        public long Find()
+        {
+            long i = 999;
+            long j;
+            List<long> results = new List<long>();
+            long val; 
+
+            while (i > 900)
+            {
+                j = i;
+
+                while (j > 900)
+                {
+                    val = i * j;
+                    if(val.IsPalindrome())
+                        results.Add(i * j);
+                    j--;
+                }
+                i--;
+            }
+
+            return results.Max();          
+        }
+    }
+
+
     public static class PalindromeExtensions
     {
-        public static bool isPalindrome(this string str)
+        public static bool IsPalindrome(this string str)
         {
             int forwardIterator = 0;
             int reverseIterator = str.Length - 1;
@@ -37,7 +65,38 @@ namespace bagOfClasses
             return false;
         }
 
-        public static bool isPalindrome(this int number)
+        public static bool IsPalindrome(this int number)
+        {
+            int[] result = number.ToString().Select(o => Convert.ToInt32(o)).ToArray();
+
+            int forwardIterator = 0;
+            int reverseIterator = result.Length - 1;
+
+            while (forwardIterator < result.Length)
+            {
+                // iterators will only ever be equal if it is an odd length integer
+                // otherwise they will "pass" each other 
+                if (forwardIterator == reverseIterator ||
+                    forwardIterator > reverseIterator)
+                {
+                    return true;
+                }
+
+                if (result[forwardIterator] == result[reverseIterator])
+                {
+                    forwardIterator++;
+                    reverseIterator--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool IsPalindrome(this long number)
         {
             int[] result = number.ToString().Select(o => Convert.ToInt32(o)).ToArray();
 
@@ -68,6 +127,4 @@ namespace bagOfClasses
             return false;
         }
     }
-
-
 }
